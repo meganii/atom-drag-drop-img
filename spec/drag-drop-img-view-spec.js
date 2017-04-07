db23891d5ca4f64492922e314267e318798e5873
meganii <yuhei24@gmail.com>
2017-04-07 22:26:11 +0900

    first commit

---
+++ spec/drag-drop-img-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DragDropImgView from '../lib/drag-drop-img-view';
+
+describe('DragDropImgView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
