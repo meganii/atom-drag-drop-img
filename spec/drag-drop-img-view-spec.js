4dee3d28a8a7c3756a2f0efc3763832cff715ed3
meganii <yuhei24@gmail.com>
2017-04-09 06:38:02 +0900

    Remove view spec

--- spec/drag-drop-img-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import DragDropImgView from '../lib/drag-drop-img-view';
-
-describe('DragDropImgView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('life');
-  });
-});
